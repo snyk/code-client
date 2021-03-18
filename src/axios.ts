52b5bd004c7600891f50a71bef50dbabdd5791d5
Arvyd Paeglit <arvyd.paeglit@snyk.io>
2021-03-18 13:11:40 +0000

    chore: cleaned comments

--- src/axios.ts
+++ src/axios.ts
@@ -7,7 +7,6 @@ const agentOptions = {
   keepAlive: true,
   maxSockets: 100, // Maximum number of sockets to allow per host. Defaults to Infinity.
   maxFreeSockets: 10,
-  // timeout: 60000, // active socket keepalive for 60 seconds
   freeSocketTimeout: 60000, // // Maximum number of sockets to leave open for 60 seconds in a free state. Only relevant if keepAlive is set to true. Defaults to 256.
   socketActiveTTL: 1000 * 60 * 10,
 };

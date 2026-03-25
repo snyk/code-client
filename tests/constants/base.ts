0f546bc4fadc79233c73265c886f2a5893ff8f92
Felix Sargent <felix.sargent@snyk.io>
2026-03-25 14:33:59 +0000

    chore(test): remove legacy auth API tests

--- tests/constants/base.ts
+++ tests/constants/base.ts
@@ -1,5 +1,4 @@
 export const baseURL = process.env.SNYK_API_HOST || 'https://deeproxy.dev.snyk.io';
-export const authHost = process.env.SNYK_AUTH_HOST || 'https://app.dev.snyk.io';
 export const sessionToken = process.env.SNYK_API_KEY || '';
 export const TEST_TIMEOUT = 100000;
 export const source = 'test-source';
